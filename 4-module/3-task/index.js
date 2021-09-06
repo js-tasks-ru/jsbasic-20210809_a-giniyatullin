afc82da81baff60d0889a8f5a448a96b08d9cb1d
a.giniyatullin <a.giniyatullin@sirius-is.ru>
2021-09-06 10:54:17 +0300

    Решение 4 модуля

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+  let row = table.rows;
+  let gender , age , status 
+  let object = {
+      m: 'male',
+      f: 'female',
+      true: 'available',
+      false: 'unavailable'
+  }
+  for (let i = 1; i < row.length; i++){
+      gender = row[i].children[2];
+      age = row[i].children[1];
+      status = row[i].children[3];
+      gender.parentNode.classList.add(object[gender.innerHTML]);
+      if(age.innerHTML < 18) {
+        gender.parentNode.setAttribute('style', 'text-decoration: line-through');
+      }
+      if (status.hasAttribute('data-available')) {
+        let data = status.getAttribute('data-available');
+        gender.parentNode.classList.add(object[data]);
+      } 
+      else {
+        gender.parentNode.hidden = true;
+      }
+  }
 }
